69ae7674e900ce3d2176b548c6715d55f2a220a4
Paul Kalupnieks <paul@shoplocket.com>
2013-03-29 10:34:00 -0400

    Rails 4 with Ruby 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edit::Application.config.secret_key_base = '2dbed96fbbe38dcf67ba75872957e23257c4fa8bbd2e576ce70026155b8a9f671fa95034aa9c0d9eb0b7e2ba8735f723ae8fe189c3854587bcaa4b5f2ff9ddc0'
